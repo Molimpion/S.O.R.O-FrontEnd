307a88a57a3eb9ffc8c066ea9b6f2714066bd0ee
VicZambom <140662019+VicZambom@users.noreply.github.com>
2025-10-10 04:28:11 +0000

    primeiro commit de configurações iniciais

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-center p-4">
+      <h1 className="text-4xl font-bold text-emerald-400">
+        Tudo Pronto!
+      </h1>
+      <p className="text-gray-300 mt-2">
+        Agora podemos focar no desenvolvimento.
+      </p>
+    </div>
+  );
+}
+
+export default App;

66a42bc39afe13506277a0ab2e22b45548db44a6
VicZambom <140662019+VicZambom@users.noreply.github.com>
2025-10-15 14:07:35 +0000

    Instalação do Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-center p-4">
-      <h1 className="text-4xl font-bold text-emerald-400">
+      <h1 class="text-3xl text-green-100 font-bold underline">
         Tudo Pronto!
       </h1>
       <p className="text-gray-300 mt-2">
